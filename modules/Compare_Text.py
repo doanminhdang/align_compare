ac77b63fa2e1f59b39c41976d2e9ed4a01bef907
doanminhdang <minhdang@linux.com>
2018-05-08 13:36:09 +0200

    Add regex to remove hyphenating effect in text

--- modules/Compare_Text.py
+++ modules/Compare_Text.py
@@ -3,12 +3,18 @@ import sys
 import os
 import csv
 import difflib
+import re
 
 def compare_text_columns(table, column1Position, column2Position):
     similarity = list()
     for i in range(len(table)):
         sourceSentence = table[i][column1Position]
         targetSentence = table[i][column2Position]
+        # Remove hyphen due to line break in German
+        sourceSentence = re.sub(r'[A-Za-z]\- +(?!und |oder |bzw\. )[a-z]', r'\1\2', sourceSentence)
+        table[i][column1Position] = sourceSentence
+        targetSentence = re.sub(r'[A-Za-z]\- +(?!und |oder |bzw\. )[a-z]', r'\1\2', targetSentence)
+        table[i][column2Position] = targetSentence
         similarity += [difflib.SequenceMatcher(None, sourceSentence, targetSentence).ratio()]
     return table, similarity
 
